d3ccb0ff987b88307dbf5a32c345c9c35eff3cd7
srosengren <sebastian@rosengren.me>
2021-01-15 20:00:07 +0100

    Selected date is now part of url/route

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,45 @@
-import './app.css';
-import Calendar from 'react-calendar';
-import React, { useState } from 'react';
+import "./app.css";
+import Calendar from "react-calendar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+  Route,
+  useParams,
+  useHistory
+} from "react-router-dom";
+
+function dateToKey(date) {
+  return date.getFullYear()+(`0${date.getMonth()+1}`.slice(-2))+date.getDate();
+}
+
+function Main() {
+  const { dateParam } = useParams();
+  const selectedDate = new Date(`${dateParam.substring(0,4)}-${dateParam.substring(4,6)}-${dateParam.substring(6,8)}`);
+  const history = useHistory();
+
+  function setSelectedDate(date) {
+    history.push(dateToKey(date));
+  }
+
+  return (
+    <>
+      <Calendar onChange={setSelectedDate} value={selectedDate} />
+      <h2>{selectedDate.toDateString()}</h2>
+    </>
+  );
+}
 
 function App() {
-
-  const [selectedDate, setSelectedDate] = useState(new Date());
-
   return (
     <div className="app">
       <h1>Mooday</h1>
-      <Calendar
-        onChange={setSelectedDate}
-        value={selectedDate}
-      />
-      <h2>{selectedDate.toDateString()}</h2>
+      <Router>
+        <Switch>
+          <Route path="/:dateParam" children={<Main />} />
+          <Redirect to={dateToKey(new Date())} />
+        </Switch>
+      </Router>
     </div>
   );
 }
